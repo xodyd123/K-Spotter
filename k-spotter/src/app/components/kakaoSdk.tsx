457e2d64b9575ab4f2fb080f3caae3583981466b
xodyd123 <gksrmf9186@naver.com>
2025-09-16 12:28:20 +0900

    검색 클릭시 장소  클러스터링 기능 구현

--- k-spotter/src/app/components/kakaoSdk.tsx
+++ k-spotter/src/app/components/kakaoSdk.tsx
@@ -9,13 +9,12 @@ export default function KakaoSDK() {
 
   return (
     <Script
-      id="kakao-sdk"
-      strategy="afterInteractive"
-      src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_JS_KEY}&autoload=false`}
-      onLoad={() => {
-        // 브라우저에서만 실행됨
-        window.dispatchEvent(new Event("kakao:loaded"));
-      }}
-    />
+    id="kakao-sdk"
+    strategy="afterInteractive"
+    src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_JS_KEY}&autoload=false&libraries=services,clusterer`}
+    onLoad={() => {
+      window.dispatchEvent(new Event("kakao:loaded"));
+    }}
+  />
   );
 }
