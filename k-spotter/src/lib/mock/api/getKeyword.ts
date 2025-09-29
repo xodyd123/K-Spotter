aa99cd775958e41364d024a7cd13cf67974b5f0e
xodyd123 <gksrmf9186@naver.com>
2025-09-29 12:33:54 +0900

    feat:이미지 클릭 기능 구현 및 ui 구현 중

--- k-spotter/src/lib/mock/api/getKeyword.ts
+++ k-spotter/src/lib/mock/api/getKeyword.ts
@@ -2,10 +2,12 @@ export async function GetKeywordSearch({keyword} : {keyword : string}){
 
    const sp =  new URLSearchParams();; 
 
-   sp.append("keyword" , keyword) ; 
+   sp.append("keyword" , keyword) ;  
 
+   
    const result =  await fetch(`/api/keyword?${sp}`) ; 
+   
+   const r = await result.json();   
 
-   return result.json() ; 
-
+   return r ; 
 }
